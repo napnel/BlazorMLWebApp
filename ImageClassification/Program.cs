5be29dde1ee5b1b27d58813662d74ba3c510ee7a
Yoshiaki Nejime <yoshi3njm@gmail.com>
2022-11-03 23:54:16 +0900

    add file uploader

--- ImageClassification/Program.cs
+++ ImageClassification/Program.cs
@@ -1,7 +1,7 @@
-﻿using ImageClassification;
+﻿// using ImageClassification;
 
-const string imageFolder = ".\\Data\\Test";
+// const string imageFolder = ".\\Data\\Test";
 
-InferenceModel inference = new InferenceModel(SqueezeNetSettings.modelPath, SqueezeNetSettings.inputColumnName, SqueezeNetSettings.outputColumnName);
-IEnumerable<ImageData> images = ImageData.ReadFromFile(imageFolder);
-inference.PredictImage(images);
+// InferenceModel inference = new InferenceModel(SqueezeNetSettings.modelPath, SqueezeNetSettings.inputColumnName, SqueezeNetSettings.outputColumnName);
+// IEnumerable<ImageData> images = ImageData.ReadFromFile(imageFolder);
+// inference.PredictImage(images);
--- Program.cs
+++ Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddHttpClient();
 builder.Services.AddMudServices();
 
 var app = builder.Build();
